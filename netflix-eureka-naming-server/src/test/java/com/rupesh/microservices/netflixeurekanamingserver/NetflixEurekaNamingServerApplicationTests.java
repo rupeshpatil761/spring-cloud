7c255923a9f93d102c04b4f9cdf861a12226dfba
Rupesh Patil <rupeshpatil.461@gmail.com>
2020-11-18 18:22:34 +0530

    Eureka Server

---
+++ netflix-eureka-naming-server/src/test/java/com/rupesh/microservices/netflixeurekanamingserver/NetflixEurekaNamingServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rupesh.microservices.netflixeurekanamingserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NetflixEurekaNamingServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
